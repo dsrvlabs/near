3d80c57e6a55768adfd4fde2349447fdac28df60
sigmoid <jiyun.kim@dsrvlabs.com>
2020-06-23 02:01:22 +0000

    sharing script for new validators
    Signed-off-by: sigmoid <jiyun.kim@dsrvlabs.com>

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #default env variable
+account_id="dsrv.betanet"
 validator_id="staking.dsrv.betanet"
 rpc_call="http post https://rpc.betanet.near.org jsonrpc=2.0"
 
-content=$($rpc_call id=dontcare method=block params:='{"finality": "final"}')
+
+content=$($rpc_call id=dontcare method=block params:='{"finality": "final"}') 
 current_epoch_id=$( jq -r '.result.header.epoch_id' <<< $content )
 height=$( jq -r '.result.header.height' <<< $content )
 epoch_start_height=$($rpc_call method=validators params:='[null]' id=dontcare | jq -r .result.epoch_start_height)
@@ -16,48 +18,54 @@ echo current block height: $height
 
 while true
 do
-        content=$($rpc_call id=dontcare method=block params:='{"finality": "final"}')
-        next_epoch_id=$( jq -r '.result.header.epoch_id' <<< $content )
-        height=$( jq -r '.result.header.height' <<< $content )
-        epoch_start_height=$($rpc_call method=validators params:='[null]' id=dontcare | jq -r .result.epoch_start_height)
+	content=$($rpc_call id=dontcare method=block params:='{"finality": "final"}') 
+	next_epoch_id=$( jq -r '.result.header.epoch_id' <<< $content )
+	height=$( jq -r '.result.header.height' <<< $content )
+	epoch_start_height=$($rpc_call method=validators params:='[null]' id=dontcare | jq -r .result.epoch_start_height)
 
-        if [ "$current_epoch_id" != "$next_epoch_id" ]; then
-                #epoch chagned: PING!
-                echo "epoch chaged, ping!"
-                near call $validator_id  ping '{}' --accountId=dsrv.betanet
+	if [ "$current_epoch_id" != "$next_epoch_id" ]; then
+		#epoch chagned: PING!
+		echo "epoch chaged, ping!" 
+		near call $validator_id  ping '{}' --accountId=$account_id
 
-                echo "epoch changed from $current to $next"
-                echo "block height is $height"
-                current=$next
+		echo "epoch changed from $current to $next"
+		echo "block height is $height"
+		current=$next
 
-                content=$($rpc_call method=validators params:='[null]' id=dontcare)p
-                vali_cur=$( jq -r '.result.current_validators' <<< $content | grep $validator_id | wc -l )
-                vali_nex=$( jq -r '.result.next_validators' <<< $content | grep $validator_id | wc -l )
-        else
-                #epoch chagned: report status
-                content=$($rpc_call method=validators params:='[null]' id=dontcare)
-                vali_cur=$( jq -r '.result.current_validators' <<< $content | grep $validator_id | wc -l )
-                vali_nex=$( jq -r '.result.next_validators' <<< $content | grep $validator_id | wc -l )
+		content=$($rpc_call method=validators params:='[null]' id=dontcare)p
+		vali_cur=$( jq -r '.result.current_validators' <<< $content | grep $validator_id | wc -l )
+		vali_nex=$( jq -r '.result.next_validators' <<< $content | grep $validator_id | wc -l )
 
-                echo "=== $validator_id validator status ==="
-                echo "current epoch $vali_cur, next epoch $vali_nex"
-                echo "1 epoch size 10000 betanet"
-                now_height=`expr $height - $epoch_start_height`
-                echo epoch pogress `expr $now_height / 100`%
+ 		#python3 dsrv_email_sender.py "current epoch $vali_cur, next epoch $vali_nex"
+	else
+		#epoch chagned: report status
+		content=$($rpc_call method=validators params:='[null]' id=dontcare)	
+		vali_cur=$( jq -r '.result.current_validators' <<< $content | grep $validator_id | wc -l )
+		vali_nex=$( jq -r '.result.next_validators' <<< $content | grep $validator_id | wc -l )
 
-
-                my_staking_status=$(near state $validator_id | awk '/locked/{print substr($2,7, length($2)-13)}')
-                current_seat_price=$(near validators current | awk '/price/ {print substr($6, 1, length($6)-2)}')
-                next_seat_price=$(near validators next | awk '/price/ {print substr($7, 1, length($7)-2)}')
-                next_next_seat_price=$(near proposals | awk '/price =/ {print substr($15, 1, length($15)-1)}')
-                echo $my_staking_status
-                echo $current_seat_price
-                echo $next_seat_price
-                echo $next_next_seat_price
-
-                #do something for challenge004
+		echo "=== $validator_id validator status ===" 
+		echo "current epoch $vali_cur, next epoch $vali_nex"
+		echo "1 epoch size 10000 betanet" 
+		now_height=`expr $height - $epoch_start_height`
+		echo epoch pogress `expr $now_height / 100`%
 
 
-        fi
-        sleep 10
+		my_staking_status=$(near state $validator_id | awk '/locked/{print substr($2,7, length($2)-13)}')
+		current_seat_price=$(near validators current | awk '/price/ {print substr($6, 1, length($6)-2)}')
+		next_seat_price=$(near validators next | awk '/price/ {print substr($7, 1, length($7)-2)}')
+		next_next_seat_price=$(near proposals | awk '/price =/ {print substr($15, 1, length($15)-1)}')
+		echo pool_locked_token $my_staking_status
+		echo t+0 seat price: $current_seat_price
+		echo t+1 seat price: $next_seat_price
+		echo t+2 seat price: $next_next_seat_price
+
+		#do something for challenge004, simply next 
+		#near call $validator_id stake '{"amount": "$next_seat_price"}' --accountId=$account_id
+
+
+	fi
+	sleep 10
 done
+
+
+
